0d97da126f4c73c093ef38927a8f84e950d9d260
Mason Barry <mbarry22@my.bcit.ca>
2023-01-26 11:40:16 -0800

    Footer Div's and items, not styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,84 @@
 import React from "react";
-
+import  '../styles/footer.css'
 export const Footer = () => {
   return (
     <>
-    <div className="footer">
-    <h1>Footer</h1>
+    <div className="footer-container">
+      <div class="footer-inside">
+        <div class="footerColumn account">
+          <h3>My Account</h3>
+          <br></br>
+          <p>Profile</p>
+          <p>CBC Gem</p>
+          <p>Newsletters</p>
+          <p>About CBC Accounts</p>
+        </div>
+        <div class="footerColumn connect">
+          <h3>Connect With CBC</h3>
+          <br></br>
+          <p>Facebook</p>
+          <p>Twitter</p>
+          <p>Youtube</p>
+          <p>Instagram</p>
+          <p>Mobile</p>
+          <p>RSS</p>
+          <p>Podcasts</p>
+        </div>
+        <div class="footerColumn contact">
+          <h3>Contact CBC</h3>
+          <br></br>
+          <p>Submit FeedBack</p>
+          <p>Help Center</p>
+          <p>Audience Relations, CBC
+             P.O. Box 500 Station A
+             Toronto, ON
+             Canada, M5W 1E6</p>
+          <p>Toll-free (Canada only):
+            <br></br>
+             1-866-306-4636</p>
+          </div>
+          <div class="footerColumn about">
+          <h3>About CBC</h3>
+          <br></br>
+          <p>Corporate Info</p>
+          <p>Siteap</p>
+          <p>Reuse & Permission</p>
+          <p>Terms of Use</p>
+          <p>Privacy</p>
+          <p>Jobs</p>
+          <p>Our Unions</p>
+          <p>Independent Producers</p>
+          <p>Political Ads Registry</p>
+          <p>AdChoices</p>
+          </div>
+          <div class="footerColumn services">
+          <h3>Services</h3>
+          <br></br>
+          <p>Ombudsman</p>
+          <p>Public Appearances</p>
+          <p>Commercial Services</p>
+          <p>CBC Shop</p>
+          <p>Doing Business with Us</p>
+          <p>Renting Facilities</p>
+          <p>Radio Canada International</p>
+          </div> 
+          <div class="footerColumn services">
+          <h3>Services</h3>
+          <br></br>
+          <p>It is a priority for CBC to 
+            <br></br>create a website that is
+            <br></br> accessible to all Canadians
+            <br></br> including people with visual,
+            <br></br> hearing, motor and cognitive
+            <br></br> challenges.</p>
+          <p>Closed Captioning and
+            <br></br> Described Video is available
+            <br></br> for many CBC shows offered
+            <br></br> on CBC Gem.</p>
+          <p>About CBC Accessibility</p>
+          <p>Accessibility Feedback</p>
+          </div> 
+      </div>
     </div>
     </>
   );
