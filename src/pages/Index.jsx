0830630b428058529589a4e2c3789eb90c8255fb
Brayden Kim <dudgjs1318@gmail.com>
2023-01-26 15:02:20 -0800

    refactorying contentList

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,32 +1,12 @@
 import React, { useEffect, useState } from "react";
 import ContentList from "../components/contentList/ContentList";
-import articles from "../components/contentList/contentList.json";
-import styles from "../components/contentList/ContentList.module.css"
 export const Index = () => {
-    const [contentLists, setContentLists] = useState([articles])
-    useEffect(() => {
-        setContentLists(articles.articles);
-    }, []);
+
     return (
         <>
             <div className="index-wrapper">
                 <h1>Hello World</h1>
-                <section className={styles.contentList__wrapper}>
-                    <div className={styles.news__section}>
-                        <h1>News Sections</h1>
-                        <span className="material-symbols-outlined">
-                            feed
-                        </span>
-                    </div>
-                    <div className={styles.contentList__subWrapper}>
-                        {contentLists.map((contentList, idx) => (
-                            <ContentList
-                                key={idx}
-                                contentList={contentList}
-                            />
-                        ))}
-                    </div>
-                </section>
+                <ContentList />
             </div>
         </>
     )
