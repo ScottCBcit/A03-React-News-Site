2e3795cb2c1de80c801aa774c78f943fb099eeb3
Scott Croin <scroin@my.bcit.ca>
2023-01-26 10:39:10 -0800

    working Router

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <>
+        <div className="header">
+        <h1>Header</h1>
+        </div>
+    </>
+  );
+};
---
+++ src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+export const Index = () => {
+
+    return (
+        <>
+        <div className="index-wrapper">
+        <h1>Hello World</h1>
+        </div>
+        </>
+    )
+    }
+
