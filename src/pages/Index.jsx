ef92031c68da26a4b195d07034508e9cefb06eae
Brayden Kim <dudgjs1318@gmail.com>
2023-01-26 14:08:06 -0800

    content list

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,29 @@
 import React, { useEffect, useState } from "react";
+import ContentList from "../components/contentList/ContentList";
+import articles from "../components/contentList/contentList.json";
+import styles from "../components/contentList/ContentList.module.css"
 export const Index = () => {
-
+    const [contentLists, setContentLists] = useState([articles])
+    useEffect(() => {
+        setContentLists(articles.articles);
+    }, []);
     return (
         <>
-        <div className="index-wrapper">
-        <h1>Hello World</h1>
-        </div>
+            <div className="index-wrapper">
+                <h1>Hello World</h1>
+                <section className={styles.contentList__wrapper}>
+                    <h1>News Sections</h1>
+                    <div className={styles.contentList__subWrapper}>
+                        {contentLists.map((contentList, idx) => (
+                            <ContentList
+                                key={idx}
+                                contentList={contentList}
+                            />
+                        ))}
+                    </div>
+                </section>
+            </div>
         </>
     )
-    }
+}
 
