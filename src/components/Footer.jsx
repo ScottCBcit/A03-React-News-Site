08654142eed1aa6ef43000eb955020d0ae473bca
Brayden Kim <dudgjs1318@gmail.com>
2023-01-27 11:03:49 -0800

    fixed position

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,93 +1,93 @@
 import React from "react";
-import  '../components/footer/footer.css'
+import '../components/footer/footer.css'
 export const Footer = () => {
   return (
     <>
-    <div className="footer-container">
-      <div class="footer-inside">
-        <div class="footerColumn account">
-          <h3>My Account</h3>
-          <br></br>
-          <a href="#"><p>Profile</p></a>
-          <a href="#"><p>CBC Gem</p></a>
-          <a href="#"><p>Newsletters</p></a>
-          <a href="#"><p>About CBC Accounts</p></a>
-        </div>
-        <div class="footerColumn connect">
-          <h3>Connect With CBC</h3>
-          <br></br>
-          <a href="#"><p>Facebook</p></a>
-          <a href="#"><p>Twitter</p></a>
-          <a href="#"><p>Youtube</p></a>
-          <a href="#"><p>Instagram</p></a>
-          <a href="#"><p>Mobile</p></a>
-          <a href="#"><p>RSS</p></a>
-          <a href="#"><p>Podcasts</p></a>
-        </div>
-        <div class="footerColumn contact">
-          <h3>Contact CBC</h3>
-          <br></br>
-          <a href="#"><p>Submit FeedBack</p></a>
-          <a href="#"><p>Help Center</p></a>
-          <p>Audience Relations, CBC
+      <div className="footer-container">
+        <div className="footer-inside">
+          <div className="footerColumn account">
+            <h3>My Account</h3>
             <br></br>
-             P.O. Box 500 Station A
-             <br></br>
-             Toronto, ON
-             <br></br>
-             Canada, M5W 1E6</p>
-          <p>Toll-free (Canada only):
+            <a href="#"><p>Profile</p></a>
+            <a href="#"><p>CBC Gem</p></a>
+            <a href="#"><p>Newsletters</p></a>
+            <a href="#"><p>About CBC Accounts</p></a>
+          </div>
+          <div className="footerColumn connect">
+            <h3>Connect With CBC</h3>
             <br></br>
-             1-866-306-4636</p>
+            <a href="#"><p>Facebook</p></a>
+            <a href="#"><p>Twitter</p></a>
+            <a href="#"><p>Youtube</p></a>
+            <a href="#"><p>Instagram</p></a>
+            <a href="#"><p>Mobile</p></a>
+            <a href="#"><p>RSS</p></a>
+            <a href="#"><p>Podcasts</p></a>
           </div>
-          <div class="footerColumn about">
-          <h3>About CBC</h3>
-          <br></br>
-          <a href="#"><p>Corporate Info</p></a>
-          <a href="#"><p>Siteap</p></a>
-          <a href="#"><p>Reuse & Permission</p></a>
-          <a href="#"><p>Terms of Use</p></a>
-          <a href="#"><p>Privacy</p></a> 
-          <a href="#"><p>Jobs</p></a> 
-          <a href="#"><p>Our Unions</p></a> 
-          <a href="#"><p>Independent Producers</p></a> 
-          <a href="#"><p>Political Ads Registry</p></a> 
-          <a href="#"><p>AdChoices</p></a> 
+          <div className="footerColumn contact">
+            <h3>Contact CBC</h3>
+            <br></br>
+            <a href="#"><p>Submit FeedBack</p></a>
+            <a href="#"><p>Help Center</p></a>
+            <p>Audience Relations, CBC
+              <br></br>
+              P.O. Box 500 Station A
+              <br></br>
+              Toronto, ON
+              <br></br>
+              Canada, M5W 1E6</p>
+            <p>Toll-free (Canada only):
+              <br></br>
+              1-866-306-4636</p>
           </div>
-          <div class="footerColumn services">
-          <h3>Services</h3>
-          <br></br>
-          <a href="#"><p>Ombudsman</p></a>
-          <a href="#"><p>Public Appearances</p></a>
-          <a href="#"><p>Commercial Services</p></a>
-          <a href="#"><p>CBC Shop</p></a>
-          <a href="#"><p>Doing Business with Us</p></a>
-          <a href="#"><p>Renting Facilities</p></a>
-          <a href="#"><p>Radio Canada International</p></a>
-          </div> 
-          <div class="footerColumn services">
-          <h3>Accessibility</h3>
-          <br></br>
-          <p>It is a priority for CBC to 
-            <br></br>create a website that is
-            <br></br> accessible to all Canadians
-            <br></br> including people with visual,
-            <br></br> hearing, motor and cognitive
-            <br></br> challenges.</p>
-          <p>Closed Captioning and
-            <br></br> Described Video is available
-            <br></br> for many CBC shows offered
-            <br></br> on CBC Gem.</p>
-            <a href="#"><p>About CBC Accessibility</p></a> 
+          <div className="footerColumn about">
+            <h3>About CBC</h3>
+            <br></br>
+            <a href="#"><p>Corporate Info</p></a>
+            <a href="#"><p>Siteap</p></a>
+            <a href="#"><p>Reuse & Permission</p></a>
+            <a href="#"><p>Terms of Use</p></a>
+            <a href="#"><p>Privacy</p></a>
+            <a href="#"><p>Jobs</p></a>
+            <a href="#"><p>Our Unions</p></a>
+            <a href="#"><p>Independent Producers</p></a>
+            <a href="#"><p>Political Ads Registry</p></a>
+            <a href="#"><p>AdChoices</p></a>
+          </div>
+          <div className="footerColumn services">
+            <h3>Services</h3>
+            <br></br>
+            <a href="#"><p>Ombudsman</p></a>
+            <a href="#"><p>Public Appearances</p></a>
+            <a href="#"><p>Commercial Services</p></a>
+            <a href="#"><p>CBC Shop</p></a>
+            <a href="#"><p>Doing Business with Us</p></a>
+            <a href="#"><p>Renting Facilities</p></a>
+            <a href="#"><p>Radio Canada International</p></a>
+          </div>
+          <div className="footerColumn services">
+            <h3>Accessibility</h3>
+            <br></br>
+            <p>It is a priority for CBC to
+              <br></br>create a website that is
+              <br></br> accessible to all Canadians
+              <br></br> including people with visual,
+              <br></br> hearing, motor and cognitive
+              <br></br> challenges.</p>
+            <p>Closed Captioning and
+              <br></br> Described Video is available
+              <br></br> for many CBC shows offered
+              <br></br> on CBC Gem.</p>
+            <a href="#"><p>About CBC Accessibility</p></a>
             <a href="#"><p>Accessibility Feedback</p></a>
-          </div> 
-          
-          
+          </div>
+
+
+        </div>
+
       </div>
 
-    </div>
 
-    
     </>
   );
 };
